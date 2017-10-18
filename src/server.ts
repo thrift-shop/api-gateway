bfeed37f1ece9ee19b70fff2fe0974f5878dd050
Nick Nance <nick.nance@creditkarma.com>
2017-10-17 22:08:07 -0700

    refactor server

--- src/server.ts
+++ src/server.ts
@@ -1,49 +1,57 @@
 import { loadSchema } from '@creditkarma/graphql-loader'
 import { executableSchemaFromModules } from '@creditkarma/graphql-loader'
 import { graphiqlHapi, graphqlHapi } from 'apollo-server-hapi'
+import { GraphQLSchema } from 'graphql'
 import { Server } from 'hapi'
 
 import { getCatalogModule } from './modules/catalog'
 
-const HOST = 'localhost'
-const PORT = 3000
+const serverConnect = (server: Server) => (schema: GraphQLSchema) => {
+    return new Promise((resolve, reject) => {
+        server.connection({
+            host: 'localhost',
+            port: 3000,
+        })
+        resolve(schema)
+    })
+}
 
-const server = new Server()
+const registerGQLRoutes = (server: Server) => (schema: GraphQLSchema) => {
+    return server.register([
+        {
+            register: graphqlHapi,
+            options: {
+                path: '/graphql',
+                graphqlOptions: { schema },
+                route: {
+                    cors: true,
+                },
+            },
+        }, {
+            register: graphiqlHapi,
+            options: {
+                path: '/',
+                graphiqlOptions: {
+                    endpointURL: '/graphql',
+                },
+            },
+        },
+    ])
+}
 
-server.connection({
-    host: HOST,
-    port: PORT,
-})
+const startServer = (server: Server) => () => {
+    server.start((err) => {
+        if (err) {
+            throw err
+        }
+        console.log(`Server running at: ${server.info ? server.info.uri : 'UNKOWN'}`)
+    })
+}
+
+const hapi = new Server()
 
 Promise.all([getCatalogModule])
     .then(executableSchemaFromModules)
-    .then((schema) => {
-        return server.register([
-            {
-                register: graphqlHapi,
-                options: {
-                    path: '/graphql',
-                    graphqlOptions: { schema },
-                    route: {
-                        cors: true,
-                    },
-                },
-            }, {
-                register: graphiqlHapi,
-                options: {
-                    path: '/',
-                    graphiqlOptions: {
-                        endpointURL: '/graphql',
-                    },
-                },
-            },
-        ])
-    })
-    .then((err) => {
-        return server.start((startErr) => {
-            if (startErr) {
-                throw startErr
-            }
-            console.log(`Server running at: ${server.info ? server.info.uri : HOST}`)
-        })
-    })
+    .then(serverConnect(hapi))
+    .then(registerGQLRoutes(hapi))
+    .then(startServer(hapi))
